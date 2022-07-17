4ce878fe2115fb869b04375e6807594d480228ce
sthefano <sthefanoss@gmail.com>
2022-07-17 18:30:58 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/microphone_fft_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.microphone_fft_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import mic_stream
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MicStreamPlugin.register(with: registry.registrar(forPlugin: "MicStreamPlugin"))
+}
